86a421289027c21d97bddcc9db1c58d78a6fc95b
the-drummist <adkramer@umass.edu>
2022-06-21 10:16:13 -0400

    mongo connectioin

---
+++ api/app/data/datastore.py
@@ -0,0 +1,15 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
+
+class Datastore:
+    client: AsyncIOMotorClient = None
+
+db = Datastore()
+
+async def get_mongo():
+	return db.client.gridfs
+
+async def connect():
+	db.client = AsyncIOMotorClient(os.environ.get('MONGO_DATABASE_HOST'))
+async def disconnect():
+	db.client.close()
