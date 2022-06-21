2c35bdc8d0dae0dd56fe2c9a749053d869fbea1f
the-drummist <adkramer@umass.edu>
2022-06-21 10:17:51 -0400

    Update devices.py

--- api/app/routers/devices.py
+++ api/app/routers/devices.py
@@ -1,46 +1,49 @@
-from fastapi import APIRouter, HTTPException, UploadFile
-from fastapi.responses import StreamingResponse
+from fastapi import APIRouter, HTTPException, UploadFile, Depends
 from motor.motor_asyncio import AsyncIOMotorGridFSBucket
-from .. import dependencies, crud, schemas, utils
+from sqlalchemy.orm import Session
+from bson.objectid import ObjectId
+import werkzeug
+import gridfs
+from .. import dependencies, crud, schemas
+from ..utils import utils, custom_response
 
 router = APIRouter(prefix='/devices')
 
 @router.put('/status', response_model=schemas.Device)
-async def set_status(status: str, device: schemas.Device = Depends(dependencies.device_authorized), session: Session = Depends(dependencies.get_session)):
-	device = crud.get_device(session, device.id)
+async def set_status(status: str, device: schemas.DeviceID = Depends(dependencies.device_authorized), session: Session = Depends(dependencies.get_session)):
+	device = crud.get_device(session, device)
 	device.status = status
 	session.commit()
 	session.refresh(device)
 	return device
 
-@router.get('/download/{mongo_id}', response_class=StreamingResponse)
-async def download_protocol(mongo_id: str, device: schemas.Device = Depends(dependencies.device_authorized), fs: AsyncIOMotorGridFSBucket = Depends(dependencies.get_fs), session: Session = Depends(dependencies.get_session)):
-	grid_out = await fs.open_download_stream(mongo_id)
-	return utils.chunk_generator(grid_out)
+@router.get('/download/{content_id}')
+async def download_protocol(content_id: str, device: schemas.DeviceID = Depends(dependencies.device_authorized), fs: AsyncIOMotorGridFSBucket = Depends(dependencies.get_fs), session: Session = Depends(dependencies.get_session)):
+	content_id = ObjectId(content_id)
+	try:
+		grid_out = await fs.open_download_stream(content_id)
+	except gridfs.NoFile:
+		raise HTTPException(status_code=404, detail='Resource not found')
+	return custom_response.AsyncIOMotorFileResponse(grid_out)
 
 @router.post('/upload', response_model=list[schemas.File])
-async def upload_files(files: list[UploadFile], device: schemas.Device = Depends(dependencies.device_authorized), fs: AsyncIOMotorGridFSBucket = Depends(dependencies.get_fs), session: Session = Depends(dependencies.get_session)):
+async def upload_files(files: list[UploadFile], device: schemas.DeviceID = Depends(dependencies.device_authorized), fs: AsyncIOMotorGridFSBucket = Depends(dependencies.get_fs), session: Session = Depends(dependencies.get_session)):
 	new_files = []
-	async for file in files:
+	for file in files:
+		file.filename = werkzeug.utils.secure_filename(file.filename)
 		file_id = await fs.upload_from_stream(file.filename, file.file)
-		file_created = schemas.FileCreate(filename=file.filename, mongo_id=file_id)
-		new_file = crud.create_file(session, file_created, device.id)
+		file_id = str(file_id)
+		file_created = schemas.FileCreate(filename=file.filename, content_id=file_id)
+		new_file = crud.create_file(session, file_created, device)
 		new_files.append(new_file)
 	return new_files
 
 @router.get('/files', response_model=list[schemas.File])
-async def get_files(device: schemas.Device = Depends(dependencies.device_authorized), session: Session = Depends(dependencies.get_session)):
-	files = crud.get_files(session, device.id)
+async def get_files(device: schemas.DeviceID = Depends(dependencies.device_authorized), session: Session = Depends(dependencies.get_session)):
+	files = crud.get_files(session, device)
 	return files
 
 @router.get('/protocols', response_model=list[schemas.Protocol])
-async def get_protocols(device: schemas.Device = Depends(dependencies.device_authorized), session: Session = Depends(dependencies.get_session)):
+async def get_protocols(device: schemas.DeviceID = Depends(dependencies.device_authorized), session: Session = Depends(dependencies.get_session)):
 	files = crud.get_protocols(session)
-	return files
-
-
-@router.delete('/files/remove', response_model=list[schemas.File])
-async def remove_files(files: list[schemas.File], device: schemas.Device = Depends(dependencies.device_authorized), session: Session = Depends(dependencies.get_session)):
-	for file in files:
-		crud.delete_file(session, file.id)
 	return files
