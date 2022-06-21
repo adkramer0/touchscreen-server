1f1b3e4d5025969113a411da8209efb6b6da06d6
the-drummist <adkramer@umass.edu>
2022-06-21 10:17:46 -0400

    Update main.py

--- api/app/main.py
+++ api/app/main.py
@@ -1,54 +1,63 @@
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, Request
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
 from .routers import users, devices
-from . import models, crud
-from .database import engine
-from .schemas import Token, UserCreate, DeviceCreate, User, Device
-from .utils import authenticate_user, authenticate_device, create_access_token
-from .dependencies import get_session
+from . import crud, dependencies, schemas
+from .utils import utils
+from .data import models, datastore, database
 
-app = FastAPI()
-
-models.Base.metadata.create_all(bind=engine)
+app = FastAPI(root_path='/api')
 
 app.include_router(users.router)
 app.include_router(devices.router)
 
-@app.post('/token', response_model=Token)
-async def login(form_data: OAuth2PasswordRequestForm = Depends(), session: Session = Depends(get_session)):
-	client = form_data.scopes[0]
+app.add_event_handler('startup', datastore.connect)
+app.add_event_handler('shutdown', datastore.disconnect)
+
+models.Base.metadata.create_all(bind=database.engine)
+utils.init_db()
+
+
+@app.post('/token', response_model=schemas.Token)
+async def login(form_data: OAuth2PasswordRequestForm = Depends(), session: Session = Depends(dependencies.get_session)):
+	try:
+		client = form_data.scopes[0]
+	except IndexError:
+		client = 'device' # temporary for testing with docs
 	login_exception = HTTPException(
         status_code=401,
         detail="Incorrect username or password",
         headers={"WWW-Authenticate": "Bearer"},
     )
 	if client == 'user':
-		user = authenticate_user(session, form_data.username, form_data.password)
+		user = await utils.authenticate_user(session, form_data.username, form_data.password)
 		if not user:
 			raise login_exception
-		access_token = create_access_token(data={'sub': client+':'+user.username})
+		access_token = utils.create_access_token(data={'sub': client+':'+user.username})
 	elif client == 'device':
-		device = authenticate_device(session, form_data.username, form_data.password)
+		device = await utils.authenticate_device(session, schemas.DeviceID(id=int(form_data.username)), form_data.password)
 		if not device:
 			raise login_exception
-		access_token = create_access_token(data={'sub': client+':'+client.id})
+		access_token = utils.create_access_token(data={'sub': client+':'+str(device.id)})
 	else:
 		raise HTTPException(status_code=400, detail='client scope unrecognized')
 	return {"access_token": access_token, "token_type": "bearer"}
 
-@app.post('/users/register', response_model=User)
-def register_user(user: UserCreate, session: Session = Depends(get_session)):
+@app.post('/users/register', response_model=schemas.User)
+def register_user(user: schemas.UserCreate, session: Session = Depends(dependencies.get_session)):
+	if user.username == 'admin':
+		raise HTTPException(status_code=409, detail='admin username reserved')
 	try:
 		new_user = crud.create_user(session, user)
-	except Exception as e:
-		raise HTTPException(status_code=500, detail=str(e))
+	except IntegrityError:
+		raise HTTPException(status_code=409, detail='username already taken')
 	return new_user
 
-@app.post('/devices/register', response_model=Device)
-def register_user(device: DeviceCreate, session: Session = Depends(get_session)):
+@app.post('/devices/register', response_model=schemas.DeviceNoFiles)
+def register_user(device: schemas.DeviceCreate, session: Session = Depends(dependencies.get_session)):
 	try:
 		new_device = crud.create_device(session, device)
-	except Exception as e:
-		raise HTTPException(status_code=500, detail=str(e))
+	except IntegrityError:
+		raise HTTPException(status_code=409, detail='name already taken')
 	return new_device
