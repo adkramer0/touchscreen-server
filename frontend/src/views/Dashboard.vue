b6dc7128e1202cd8ee5d7dfee98ae147df2001f6
the-drummist <adkramer@umass.edu>
2022-07-12 15:59:51 -0400

    Create Dashboard.vue

---
+++ frontend/src/views/Dashboard.vue
@@ -0,0 +1,80 @@
+<template>
+	<section>
+		<div class="card border-info mb-3">
+			<div class="card-header">
+				<h5 style="display: inline; float: inline-start;">Your Devices</h5>
+				<div style="display:  inline; float:	inline-end;">
+					<button @click="getDevices" class="btn btn-outline-info" type="button">
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+							<path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+							<path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+						</svg>
+					</button>
+				</div>
+			</div>
+			<div class="card-body">
+				<div v-if="Array.isArray(devices) && devices.length">
+					<table class="table table-info table-bordered table-striped table-hover">
+						<thead>
+							<tr>
+								<th>Name</th>
+								<th>Status</th>
+								<th>Files</th>
+								<th>Settings</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr v-for="device in devices" :key="device.id">
+								<td>{{ device.name }} </td>
+								<td v-if="device.online">
+									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" color="green" stroke="white" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+										<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+									</svg>
+								</td>
+								<td v-else>
+									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" color="red" stroke="white" fill="currentColor" class="bi bi-dash-circle-fill" viewBox="0 0 16 16">
+										<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-7z"/>
+									</svg>
+								</td>
+								<td>
+									<router-link :to="{name: 'DeviceFiles', params:{id:device.id}}">
+										<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-text-fill" viewBox="0 0 16 16">
+											<path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM5 4h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zM5 8h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1zm0 2h3a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1z"></path>
+										</svg>
+									</router-link>
+								</td>
+								<td>
+									<router-link :to="{name: 'DeviceSettings', params:{id:device.id}}">
+										<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear-wide-connected" viewBox="0 0 16 16">
+											<path d="M7.068.727c.243-.97 1.62-.97 1.864 0l.071.286a.96.96 0 0 0 1.622.434l.205-.211c.695-.719 1.888-.03 1.613.931l-.08.284a.96.96 0 0 0 1.187 1.187l.283-.081c.96-.275 1.65.918.931 1.613l-.211.205a.96.96 0 0 0 .434 1.622l.286.071c.97.243.97 1.62 0 1.864l-.286.071a.96.96 0 0 0-.434 1.622l.211.205c.719.695.03 1.888-.931 1.613l-.284-.08a.96.96 0 0 0-1.187 1.187l.081.283c.275.96-.918 1.65-1.613.931l-.205-.211a.96.96 0 0 0-1.622.434l-.071.286c-.243.97-1.62.97-1.864 0l-.071-.286a.96.96 0 0 0-1.622-.434l-.205.211c-.695.719-1.888.03-1.613-.931l.08-.284a.96.96 0 0 0-1.186-1.187l-.284.081c-.96.275-1.65-.918-.931-1.613l.211-.205a.96.96 0 0 0-.434-1.622l-.286-.071c-.97-.243-.97-1.62 0-1.864l.286-.071a.96.96 0 0 0 .434-1.622l-.211-.205c-.719-.695-.03-1.888.931-1.613l.284.08a.96.96 0 0 0 1.187-1.186l-.081-.284c-.275-.96.918-1.65 1.613-.931l.205.211a.96.96 0 0 0 1.622-.434l.071-.286zM12.973 8.5H8.25l-2.834 3.779A4.998 4.998 0 0 0 12.973 8.5zm0-1a4.998 4.998 0 0 0-7.557-3.779l2.834 3.78h4.723zM5.048 3.967c-.03.021-.058.043-.087.065l.087-.065zm-.431.355A4.984 4.984 0 0 0 3.002 8c0 1.455.622 2.765 1.615 3.678L7.375 8 4.617 4.322zm.344 7.646.087.065-.087-.065z"/>
+										</svg>
+									</router-link>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div class="alert alert-info" role="alert" v-else>
+					<h4 class="alert-heading">No Registered Devices</h4>
+					<p>You can verify devices using the Devices tab</p>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+	import {mapActions, mapGetters} from 'vuex';
+	export default {
+		name: 'Dashboard',
+		created: function() {
+			return this.getDevices()
+		},
+		computed: {
+			...mapGetters({devices: 'stateDevices'}),
+		},
+		methods: {
+			...mapActions(['getDevices']),
+		},
+	};
+</script>
