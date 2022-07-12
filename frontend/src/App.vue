f219f0f4b2e895ff61bee2ecec54809aa5fc90e3
the-drummist <adkramer@umass.edu>
2022-07-12 15:13:15 -0400

    Create App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <NavBar/>
+    <div class="container">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+  import NavBar from '@/components/NavBar.vue';
+  export default {
+    components: {
+      NavBar,
+    },
+  };
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background-color: #f1f7fc;
+  color: #2c3e50;
+  flex-grow:  1;
+}
+html {
+  background-color: #f1f7fc;
+}
+</style>
