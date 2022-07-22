7814d4c171c3140ab6be44cb5ace65662edc19ca
the-drummist <adkramer@umass.edu>
2022-07-22 10:14:43 -0400

    logout expired tokens/flash http exceptions

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -14,7 +14,22 @@ Vue.config.productionTip = false;
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = 'http://localhost/api/'; //development
 
+axios.interceptors.response.use(undefined, function(error) {
+  if (error) {
+    if (error.response.status === 401 && !error.config._retry && !(error.config.url === 'token' || error.config.url === 'users/register')) {
+      error.config._retry = true;
+      store.dispatch('resetDeviceState');
+      store.dispatch('resetUserState');
+      store.dispatch('deleteCookie');
+      router.push('/login');
+      return Promise.reject(error);
 
+    } else {
+      store.dispatch('flash', error.response);
+      return Promise.reject(error);
+    }
+  }
+});
 
 new Vue({
   store,
